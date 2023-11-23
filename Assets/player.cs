96e31eac64248c6a8147dbcf7490330d31e5509a
lukabartlo <l.bartolo@lahorde.school>
2023-11-23 13:37:45 +0100

    push2

--- Assets/player.cs
+++ Assets/player.cs
@@ -7,9 +7,11 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D CC;
-    //private Bool _OnGround = false;
+    private bool OnGround = false;
 
-    [SerializeField] private InputActionReference Left, Right, Up;
+    [SerializeField] private InputActionReference Left, Right, Up, Shoot;
+    [SerializeField] private GameObject Projectile;
+
     void Start()
     {
         CC = GetComponent<Rigidbody2D>();
@@ -26,19 +28,24 @@ public class Player : MonoBehaviour
         {
             CC.velocity = new Vector2(10, CC.velocity.y);
         }
-        if (Up.action.inProgress)
-        {
+        if (OnGround == true && Up.action.inProgress)
+        { 
             CC.velocity = new Vector2(CC.velocity.x, 20);
         }
+        if (Shoot.action.inProgress)
+        {
+            GameObject newproj = Instantiate(Projectile);
+        }
+        CC.velocity -= new Vector2(0.5f * CC.velocity.normalized.x, 0);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        // _OnGround = true;
+        OnGround = true;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-
+        OnGround = false;
     }
 }
