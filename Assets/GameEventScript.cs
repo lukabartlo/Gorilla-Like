fa0c7d5fa9c9e76d270aaf4b05ae568d080d4621
lukabartlo <l.bartolo@lahorde.school>
2023-12-01 16:59:58 +0100

    Wind push 1

--- Assets/GameEventScript.cs
+++ Assets/GameEventScript.cs
@@ -13,6 +13,7 @@ public class GameEventScript : MonoBehaviour
     public TextMeshProUGUI TurnPlayerText;
 
     private GameObject player;
+    private GameObject WindSprite;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class GameEventScript : MonoBehaviour
         player = GameObject.Find("Player");
         TimeText = GameObject.Find("CanvasText").transform.Find("TimerText").GetComponent<TextMeshProUGUI>();
         TurnPlayerText = GameObject.Find("CanvasText").transform.Find("TurnText").GetComponent<TextMeshProUGUI>();
+        WindSprite = GameObject.Find("WindSprite");
     }
 
     // Update is called once per frame
@@ -42,6 +44,34 @@ public class GameEventScript : MonoBehaviour
             Timer = 10f;
         }
         player.GetComponent<Player>().TimerOver = PlayerTurn;
+
+        Sprite newsprite = null;
+       
+        switch ((int)wind.x)
+        {
+            case -3:
+                newsprite = Resources.Load<Sprite>("LeftWindArrow 3");
+                break;
+            case -2:
+                newsprite = Resources.Load<Sprite>("LeftWindArrow 2");
+                break;
+            case -1:
+                newsprite = Resources.Load<Sprite>("LeftWindArrow 1");
+                break;
+            case 0:
+                newsprite = Resources.Load<Sprite>("NoWind");
+                break;
+            case 1:
+                newsprite = Resources.Load<Sprite>("RightWindArrow 1");
+                break;
+            case 2:
+                newsprite = Resources.Load<Sprite>("RightWindArrow 2");
+                break;
+            case 3:
+                newsprite = Resources.Load<Sprite>("RightWindArrow 3");
+                break;
+        }
+        WindSprite.GetComponent<SpriteRenderer>().sprite = newsprite;
     }
 
     void DisplayTime(float TimeToDisplay)
